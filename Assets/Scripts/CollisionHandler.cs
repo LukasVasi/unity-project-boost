dee0abdb8cffbcfdb3e276b0f4e6298bbe339bd4
Lukas Vasiliauskas <lukas.vasi@gmail.com>
2024-08-20 17:52:21 +0300

    Added state management to prevent duplicate collision handling, changed rocket model

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(Collider), typeof(PlayerController), typeof(AudioSource))]
 public class CollisionHandler : MonoBehaviour
 {
-    [SerializeField] private float m_handlingDelay = 1.5f;
+    [SerializeField] private float m_levelLoadDelay = 1.5f;
 
     [Header("SFX")]
     [SerializeField] private AudioClip m_crashSound;
@@ -15,6 +15,8 @@ public class CollisionHandler : MonoBehaviour
 
     private AudioSource m_audioSource;
     private PlayerController m_playerController;
+
+    private bool m_isTransitioning = false;
 
     private void Awake()
     {
@@ -24,41 +26,56 @@ public class CollisionHandler : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (m_isTransitioning) return;
+
         switch (collision.gameObject.tag) 
         {
             case "Safe":
                 Debug.Log("Collided with a safe object");
                 break;
             case "Goal":
-                m_playerController.enabled = false;
-                m_audioSource.PlayOneShot(m_successSound);
-                Debug.Log("You've completed the level");
-                Invoke("HandleComplete", m_handlingDelay);
+                HandleComplete();
                 break;
             default:
-                m_playerController.enabled = false;
-                m_audioSource.PlayOneShot(m_crashSound);
-                Debug.Log("You've crashed");
-                Invoke("HandleCrash", m_handlingDelay);
+                HandleCrash();
                 break;
         }
     }
 
     private void HandleComplete()
     {
+        if (m_isTransitioning) return;
+
+        m_isTransitioning = true;
+
+        m_playerController.enabled = false;
+        m_audioSource.Stop();
+        m_audioSource.PlayOneShot(m_successSound);
+        Debug.Log("You've completed the level");
+
         int currentLevelBuildIndex = SceneManager.GetActiveScene().buildIndex;
         int nextLevelBuildIndex = SceneManager.sceneCountInBuildSettings <= currentLevelBuildIndex + 1 ? 0 : currentLevelBuildIndex + 1;
-        LoadLevel(nextLevelBuildIndex);
+        StartCoroutine(LoadLevelAfterDelay(nextLevelBuildIndex));
     }
 
     private void HandleCrash()
     {
+        if (m_isTransitioning) return;
+
+        m_isTransitioning = true;
+        
+        m_playerController.enabled = false;
+        m_audioSource.Stop();
+        m_audioSource.PlayOneShot(m_crashSound);
+        Debug.Log("You've crashed");
+
         int currentLevelBuildIndex = SceneManager.GetActiveScene().buildIndex;
-        LoadLevel(currentLevelBuildIndex);
+        StartCoroutine(LoadLevelAfterDelay(currentLevelBuildIndex));
     }
 
-    private void LoadLevel(int levelBuildIndex)
+    private IEnumerator LoadLevelAfterDelay(int levelBuildIndex)
     {
+        yield return new WaitForSeconds(m_levelLoadDelay);
         SceneManager.LoadScene(levelBuildIndex);
     }
 }
